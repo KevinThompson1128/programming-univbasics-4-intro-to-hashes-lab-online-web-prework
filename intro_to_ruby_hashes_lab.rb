0adec256a918dfe84dde31b651a9f6c44d529d59
Kevin Thompson <indelible11@yahoo.com>
2019-10-27 23:09:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,5 +25,7 @@ end
 def update_counting_hash(hash, key)
   if update_hash[:apples]
      update_hash[:apples] += 1
-     update_hash[:oranges] = 1
+  else   
+     update_hash[:apples] = 1
+  end   
 end
